f19b3a47919f60a91c388e1f14b7780c0aa6cc99
Masaya Yamamoto <masaya1122baseball@gmail.com>
2025-09-07 19:57:33 +0900

    ヘッダー、フッターを作成

--- coffee-app/resources/js/Pages/Home.jsx
+++ coffee-app/resources/js/Pages/Home.jsx
@@ -1,11 +1,74 @@
-import { Box, Heading, VStack, HStack, Image, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Heading,
+  VStack,
+  HStack,
+  Image,
+  Text,
+  IconButton,
+  Menu,
+  MenuList,
+  MenuButton,
+  MenuItem,
+  Link,
+} from '@chakra-ui/react';
 import React from 'react';
-import { StarIcon } from '@chakra-ui/icons';
+import { HamburgerIcon, StarIcon, SettingsIcon } from '@chakra-ui/icons';
 
 const Home = (props) => {
   return (
     <>
-      <Box m={4}>
+      <Box bg={'orange.600'}>
+        {/* ヘッダー */}
+        <HStack
+          justifyContent={'space-between'}
+          alignItems={'center'}
+          py={{ base: 0, md: 3 }}
+          px={{ base: 1, md: 2 }}
+        >
+          <Heading as="h1" size={{ base: 'xs', md: 'md' }} color={'white'}>
+            <Link
+              display={'flex'}
+              alignItems={'center'}
+              href="/home"
+              _hover={{ color: 'gray.500' }}
+            >
+              <Image boxSize="60px" src="coffeeIcon.svg" alt="CoffeeApp" />
+              {import.meta.env.VITE_APP_NAME}
+            </Link>
+          </Heading>
+          {/* PC表示 */}
+          <HStack display={{ base: 'none', md: 'flex' }} color={'white'}>
+            <Link href="#" _hover={{ color: 'gray.500' }}>
+              マイページ
+            </Link>
+            <Link href="#" _hover={{ color: 'gray.500' }}>
+              店舗登録
+            </Link>
+          </HStack>
+          {/* SP表示 */}
+          <Box
+            display={{ base: 'block', md: 'none' }}
+            px={{ base: '1', md: 'none' }}
+            py={{ base: '2', md: 'none' }}
+          >
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                aria-label="Options"
+                icon={<HamburgerIcon />}
+                variant="outline"
+                colorScheme="white"
+              />
+              <MenuList>
+                <MenuItem icon={<SettingsIcon />}>マイページ</MenuItem>
+                <MenuItem>店舗登録</MenuItem>
+              </MenuList>
+            </Menu>
+          </Box>
+        </HStack>
+      </Box>
+      <Box p={4}>
         <Heading fontSize={{ base: '24px', md: '40px', lg: '56px' }} mb={2}>
           ショップ一覧
         </Heading>
@@ -71,6 +134,20 @@ const Home = (props) => {
             </Box>
           ))}
         </VStack>
+      </Box>
+      {/* フッター */}
+      <Box>
+        <Box
+          bg={'orange.600'}
+          color={'white'}
+          fontWeight={'bold'}
+          textAlign={'center'}
+          py={{ base: 2, md: 3 }}
+        >
+          <Text fontSize={{ base: 13, md: 16 }}>
+            &copy; 2025 {import.meta.env.VITE_APP_NAME}
+          </Text>
+        </Box>
       </Box>
     </>
   );
