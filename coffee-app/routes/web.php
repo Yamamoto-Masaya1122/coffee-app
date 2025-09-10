9bf8ea74b661b475c35b65a7ebbc68bece32c6a4
Masaya Yamamoto <masaya1122baseball@gmail.com>
2025-09-10 23:46:02 +0900

    ログイン後でないとレビュー機能を使用できないように修正

--- coffee-app/routes/web.php
+++ coffee-app/routes/web.php
@@ -7,15 +7,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-// Route::get('/', function () {
-//     return Inertia::render('Welcome', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         'laravelVersion' => Application::VERSION,
-//         'phpVersion' => PHP_VERSION,
-//     ]);
-// });
-
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -30,9 +21,14 @@ Route::get('/sample', function () {
     return Inertia::render('Sample');
 })->name('sample');
 
+// 店舗関連
 Route::get('/', [ShopController::class, 'index'])->name('shop.index');
 Route::get('/shop/{id}', [ShopController::class, 'detail'])->name('shop.detail');
-Route::get('/review/create/shop/{id}', [ReviewController::class, 'create'])->name('review.create');
-Route::post('/review/store', [ReviewController::class, 'store'])->name('review.store');
+
+// レビュー関連
+Route::middleware('auth')->group(function () {
+    Route::get('/review/create/shop/{id}', [ReviewController::class, 'create'])->name('review.create');
+    Route::post('/review/store', [ReviewController::class, 'store'])->name('review.store');
+});
 
 require __DIR__ . '/auth.php';
